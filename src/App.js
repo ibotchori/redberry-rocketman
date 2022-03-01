7de498341bf22654e764f26924bcd87adfab6bf0
Irakli <koregm@gmail.com>
2022-03-01 15:56:50 +0400

    Add personal page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Landing from "./pages/Landing/Landing";
+import PersonalInformation from "./pages/PersonalInformation/PersonalInformation";
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <PersonalInformation />
     </div>
   );
 }
---
+++ src/pages/PersonalInformation/PersonalInformation.js
@@ -0,0 +1,50 @@
+import styles from "./PersonalInformation.module.css";
+
+const PersonalInformation = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainLeft}>
+        <h1 className={styles.title}>
+          Hey, Rocketeer, what are your coordinates?
+        </h1>
+        <div>
+          <form className={styles.form} action="/action_page.php">
+            <input
+              type="text"
+              id="fname"
+              name="firstName"
+              placeholder="First Name"
+            />
+            <input
+              type="email"
+              id="lname"
+              name="lstName"
+              placeholder="Last Name"
+            />
+            <input type="text" id="email" name="email" placeholder="E mail" />
+            <input
+              type="number"
+              id="number"
+              name="phone"
+              placeholder="+995 5__ __ __ __"
+            />
+          </form>
+        </div>
+      </div>
+      <div className={styles.mainRight}>
+        <h1 className={styles.title}>Redberry Origins</h1>
+        <div className={styles.rightP}>
+          <p>
+            You watch “What? Where? When?” Yeah. Our founders used to play it.
+            That’s where they got a question about a famous American author and
+            screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the
+            exact name and he answered Ray Redberry. And at that moment, a name
+            for a yet to be born company was inspired - Redberry 😇
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInformation;
