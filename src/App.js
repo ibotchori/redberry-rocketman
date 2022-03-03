720fdbe07edebf8c31b44b4cc6a881037caa33f1
Irakli <koregm@gmail.com>
2022-03-03 20:47:21 +0400

    Show Pagination on specific pages

--- src/App.js
+++ src/App.js
@@ -15,21 +15,29 @@ import {
 } from "react-router-dom";
 
 function App() {
+  let location = useLocation();
+
+  console.log(location.pathname);
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/covid" element={<Covid />} />
-          <Route path="/personalInfo" element={<PersonalInformation />} />
-          <Route path="/skills" element={<TechnicalSkill />} />
-          <Route path="/redberrian" element={<RedberrianInsights />} />
-          <Route path="/submit" element={<Submit />} />
-          <Route path="/submitted-item" element={<SubmittedItem />} />
-          <Route path="/thanks" element={<Thanks />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/personalInfo" element={<PersonalInformation />} />
+        <Route path="/skills" element={<TechnicalSkill />} />
+        <Route path="/covid" element={<Covid />} />
+        <Route path="/redberrian" element={<RedberrianInsights />} />
+        <Route path="/submit" element={<Submit />} />
+        <Route path="/submitted-item" element={<SubmittedItem />} />
+        <Route path="/thanks" element={<Thanks />} />
+      </Routes>
+      {location.pathname === "/personalInfo" ||
+      location.pathname === "/skills" ||
+      location.pathname === "/covid" ||
+      location.pathname === "/redberrian" ? (
         <Pagination />
-      </Router>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
