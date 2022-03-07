a69e7abba2a0697c92a370565f4b25cbb2731d96
Irakli <koregm@gmail.com>
2022-03-08 01:23:56 +0400

    Add logic, that allowed only next button (circle) click

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -11,73 +11,81 @@ const Pagination = () => {
 
   // Global state
   const [userInfo, setUserInfo] = useContext(UserContext);
+  console.log(location.pathname);
 
   const skillButtonClick = () => {
-    if (location.pathname !== "/personalInfo") {
-      navigate("/skills");
-    }
-    if (
-      userInfo.first_name.length < 2 ||
-      userInfo.last_name.length < 2 ||
-      regEmail.test(userInfo?.email) === false ||
-      userInfo.phone.length < 9
-    ) {
-      setUserInfo({ ...userInfo, showError: true });
-    } else if (
-      userInfo.first_name !== "" &&
-      userInfo.last_name !== "" &&
-      userInfo.email !== "" &&
-      userInfo.phone !== ""
-    ) {
-      setSkillButtonStyle(styles.active);
-      navigate("/skills");
+    if (location.pathname === "/personalInfo") {
+      if (
+        userInfo.first_name.length < 2 ||
+        userInfo.last_name.length < 2 ||
+        regEmail.test(userInfo?.email) === false ||
+        userInfo.phone.length < 9
+      ) {
+        setUserInfo({ ...userInfo, showError: true });
+      } else if (
+        userInfo.first_name !== "" &&
+        userInfo.last_name !== "" &&
+        userInfo.email !== "" &&
+        userInfo.phone !== ""
+      ) {
+        setSkillButtonStyle(styles.active);
+        navigate("/skills");
+      }
+    } else {
+      return;
     }
   };
 
   const covidButtonClick = () => {
-    if (location.pathname !== "/skills") {
-      navigate("/covid");
-    }
-    if (userInfo.skills.length < 1) {
-      setUserInfo({ ...userInfo, showSkillError: true });
+    if (location.pathname === "/skills") {
+      if (userInfo.skills.length < 1) {
+        setUserInfo({ ...userInfo, showSkillError: true });
+      } else {
+        setCovidButtonStyle(styles.active);
+        navigate("/covid");
+      }
     } else {
-      setCovidButtonStyle(styles.active);
-      navigate("/covid");
+      return;
     }
   };
 
   const redberrianButtonClick = () => {
-    if (location.pathname !== "/covid") {
-      navigate("/redberrian");
-    }
-    if (userInfo.work_preference === "") {
-      setUserInfo({ ...userInfo, showCovidError: true });
-    } else if (userInfo.had_covid === true && userInfo.had_covid_at === "") {
-      setUserInfo({ ...userInfo, showCovidError: true });
-    } else if (userInfo.vaccinated === true && userInfo.vaccinated_at === "") {
-      setUserInfo({ ...userInfo, showCovidError: true });
+    if (location.pathname === "/covid") {
+      if (userInfo.work_preference === "") {
+        setUserInfo({ ...userInfo, showCovidError: true });
+      } else if (userInfo.had_covid === true && userInfo.had_covid_at === "") {
+        setUserInfo({ ...userInfo, showCovidError: true });
+      } else if (
+        userInfo.vaccinated === true &&
+        userInfo.vaccinated_at === ""
+      ) {
+        setUserInfo({ ...userInfo, showCovidError: true });
+      } else {
+        setRedberrianButtonStyle(styles.active);
+        navigate("/redberrian");
+      }
     } else {
-      setRedberrianButtonStyle(styles.active);
-      navigate("/redberrian");
+      return;
     }
   };
 
   const submitButtonClick = () => {
-    if (location.pathname !== "/redberrian") {
-      navigate("/submit");
-    }
-    if (
-      userInfo.something_special.length < 10 ||
-      userInfo.will_organize_devtalk === "undefined"
-    ) {
-      setUserInfo({ ...userInfo, showRedberrianError: true });
-    } else if (
-      userInfo.will_organize_devtalk === true &&
-      userInfo.devtalk_topic.length < 10
-    ) {
-      setUserInfo({ ...userInfo, showRedberrianError: true });
+    if (location.pathname === "/redberrian") {
+      if (
+        userInfo.something_special.length < 10 ||
+        userInfo.will_organize_devtalk === "undefined"
+      ) {
+        setUserInfo({ ...userInfo, showRedberrianError: true });
+      } else if (
+        userInfo.will_organize_devtalk === true &&
+        userInfo.devtalk_topic.length < 10
+      ) {
+        setUserInfo({ ...userInfo, showRedberrianError: true });
+      } else {
+        navigate("/submit");
+      }
     } else {
-      navigate("/submit");
+      return;
     }
   };
 
